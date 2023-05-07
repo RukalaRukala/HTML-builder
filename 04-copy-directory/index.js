2d31dbc12c74f45a1f40778374e2a6a701e24123
Mikalai Rusakovich <kolyos83@mail.ru>
2023-05-07 22:20:56 +0300

    fix: solve the fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
+
+
+fs.mkdir('04-copy-directory/files-copy', {recursive: true}, (err) => { if (err) throw err; });
+
+fs.readdir('04-copy-directory/files-copy', (err, data) => {
+  data.forEach(file => {
+    fs.unlink('04-copy-directory/files-copy/' + file, () => {});
+  });
+});
+
+fs.readdir('04-copy-directory/files', (err, data) => {
+  data.forEach(file => {
+    fs.copyFile('04-copy-directory/files/' + file, '04-copy-directory/files-copy/' + file, () => {});
+  });
+})
 
